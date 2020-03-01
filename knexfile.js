ff06e5397641a1bf71c50a925a4a144ae6754599
MVRigonati <marcus.rigonati@hotmail.com>
2020-03-01 20:58:50 -0300

    Adicionando chave para geracao de token

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,8 @@
 module.exports = {
     client: 'postgresql',
     connection: {
+        host: '192.168.0.53',
+        port: '5432',
         database: 'tasks',
         user: 'postgres',
         password: '123456'
